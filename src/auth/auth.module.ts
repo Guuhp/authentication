7f099a223bed81d05310d3d436ea8a2024676a85
Guuhp <lg645471@gmail.com>
2023-02-02 03:48:04 -0400

    feat(auth) implement login

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,4 +1,4 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { JwtModule } from '@nestjs/jwt';
 import { PassportModule } from '@nestjs/passport';
@@ -8,17 +8,22 @@ import { LocalAuthGuard } from './guard/local-auth.guard';
 import { LocalStrategy } from './strategies/local.strategy';
 import { UserModule } from 'src/user/user.module';
 import { JwtStrategy } from './strategies/jwt.strategy';
+import { LoginValidationMiddleware } from './middleware/login-validation.middleware';
 
 @Module({
-  imports:[
+  imports: [
     PassportModule,
     UserModule,
     JwtModule.register({
       secret: process.env.JWT_SECRET,
-      signOptions:{expiresIn:'30d'}
+      signOptions: { expiresIn: '30d' }
     })
   ],
-  providers: [AuthService,LocalStrategy,JwtStrategy],
+  providers: [AuthService, LocalStrategy, JwtStrategy],
   controllers: [AuthController]
 })
-export class AuthModule {}
+export class AuthModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(LoginValidationMiddleware).forRoutes('login');
+  }
+}

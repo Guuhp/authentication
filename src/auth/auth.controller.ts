1aabcf3523eb804fe07a25a28a8a034bde7e0c49
Guuhp <lg645471@gmail.com>
2023-02-01 23:23:47 -0400

    feat(auth) implement login, guard and localStrategy

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,18 @@
+import { Body, Controller, HttpCode, HttpStatus, Post, UseGuards } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { LocalAuthGuard } from './guard/local-auth.guard';
+
+@Controller()
+export class AuthController {
+
+  constructor(private readonly authService:AuthService){}
+
+
+  @Post('login')
+  @HttpCode(HttpStatus.OK)
+  @UseGuards(LocalAuthGuard)
+  async login(){
+    this.authService.login()
+  }
+
+}
